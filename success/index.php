4ee612df91658088932938f3d159565383605620
Ashley Robinson <ashley@reassemble.ca>
2014-09-04 06:25:10 -0700

    Axing dated reference to hotmail & yahoo

--- success/index.php
+++ success/index.php
@@ -10,13 +10,8 @@
 
 	<div class="main-content">
 		<p>Your email has been added to our system and you will be e-mailed shortly with a request to confirm your subscription.</p>
-		<ul>
-		<li>You must confirm your subscription if you want to receive e-mails from The Tyee (thetyee@response.thetyee.ca).</li>
-		<br />
-		<li>Please take a moment to do that now. You may want to look in your junk folder if you can't find the message at first.</li>
-		</ul>
-		<p>Also, if you'd like to ensure that e-mails from TheTyee.ca get to your inbox, please add us to your address book. If you use a free e-mail service like Hotmail or Yahoo!, this will help to ensure that future e-mails from The Tyee don't end up in your junk folder.</p>
-
+		<p>You must confirm your subscription if you want to receive e-mails from The Tyee (thetyee@response.thetyee.ca).</p>
+		<p>Please take a moment to do that now. You may want to look in your junk folder if you can't find the message at first.</p>
 	</div>
 
 <?php
