ac3aa7bb36ee5acc7116d1c8441a543183bbc2d1
Phillip Smith <ps@phillipadsmith.com>
2015-05-07 22:53:15 -0700

    Google Tag Manager

--- inc/header.php
+++ inc/header.php
@@ -28,6 +28,14 @@ else {
 </head>
 
 <body>
-
+<!-- Google Tag Manager -->
+<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-MSQQB3"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MSQQB3');</script>
+<!-- End Google Tag Manager -->
   <div role="main">
     <img src="http://thetyee.ca/ui/img/logo.png" class="logo">
