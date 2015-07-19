7c1d69159e7ff665f4bbf5254368faecac19576e
Sally Poulsen <sally@Sallys-MacBook-Pro.local>
2015-07-18 19:14:51 -0600

    Adds new directory for national landing page. Changes syntax changing whether the updateform variable exists online 33 of the footer; it was failing locally.

--- index.php
+++ index.php
@@ -1,15 +1,13 @@
 <?php
 
-	$title = "Join The Tyee";
+  $title = "Join The Tyee";
   $form = true;
-
-  include("inc/header.php");
-  //include("$_SERVER[DOCUMENT_ROOT]/inc/header.php");
+  include("$_SERVER[DOCUMENT_ROOT]/inc/header.php");
 
 ?>
 
 
-  <h1>Get The Tyee National Newsletter <span class="label">NEW!</span></h1>
+  <h1>Join The Tyee's mailing list</h1>
   <form action="http://whatcounts.com/bin/listctrl" method="POST">
     <input type="hidden" name="slid" value="6B5869DC547D3D46CFC60A3DF69EE7EB" />
     <input type="hidden" name="cmd" value="subscribe" />
@@ -20,7 +18,29 @@
 
     <label class="email">Email: <input type="email" name="email" id="email" required aria-required="true"/></label>
 
-  
+    <label class="checkbox">
+      <div role="presentation" class="form-checkbox">
+        <div class="radio-buttons">
+          <input type="radio" aria-checked="false" id="custom_pref_enews_weekly-yes" name="custom_pref_enews_weekly" value="1"/> Yes
+          <input type="radio" aria-checked="false" id="custom_pref_enews_weekly-no" name="custom_pref_enews_weekly" value="0" checked /> No
+        </div>
+      </div>Send me the weekly newsletter
+      <div class="description">
+          Every Monday you'll get an email listing all stories from the last week, as well as polls, top comments and contests. You'll also get early notice on Tyee events, news, promotions, partner messages and special initiatives.
+        </div>
+    </label>
+
+    <label class="checkbox">
+      <div role="presentation" class="form-checkbox">
+        <div class="radio-buttons">
+          <input type="radio" aria-checked="false" id="custom_pref_enews_daily-yes" name="custom_pref_enews_daily" value="1"/> Yes
+          <input type="radio" aria-checked="true" id="custom_pref_enews_daily-no" name="custom_pref_enews_daily" value="0" checked /> No
+        </div>
+      </div>Send me the daily newsletter
+     <div class="description">
+        Every day you'll get an email with all stories from the last 24 hours, as well as polls and contests. You'll also get early notice on Tyee events, news, promotions, partner messages and special initiatives.
+      </div>
+    </label>
 
     <label class="checkbox">
       <div role="presentation" class="form-checkbox">
@@ -30,27 +50,30 @@
         </div>
       </div>Send me the national newsletter 
       <div class="description">
-     Sent out once a week, you’ll get our latest and best reporting on issues of national scope, plus occasional breaking story alerts. You'll also get early notice on Tyee events, news, promotions, partner messages and special initiatives.</div>
+     Sent out once a week, you’ll get our latest and best reporting on issues of national scope, plus occasional breaking story alerts. You'll also get early notice on Tyee events, news, promotions, partner messages and special initiatives.  </div>
     </label>
 
-  
+    <label class="checkbox">
+      <div role="presentation" class="form-checkbox">
+        <div class="radio-buttons">
+          <input type="radio" aria-checked="false" id="custom_pref_sponsors-yes" name="custom_pref_sponsor_casl" value="1"/> Yes
+          <input type="radio" aria-checked="true" id="custom_pref_sponsors-no" name="custom_pref_sponsor_casl" value="0" checked /> No
+        </div>
+      </div>Send me messages from select Tyee sponsors
+      <div class="description">
+        Messages from select sponsors we think you might be interested in. Simply having you on this list is a no-cost way to help fund more indie journalism.
+      </div>
+    </label>
 
    <input type="submit" value="Join">
-    <div class="differentiator">
-      <h2>How is this newsletter different from The Tyee’s other newsletters?</h2>
-      <p>In 2013, Tyee readers raised over $100,000 for The Tyee to hire a reporter in Ottawa and to expand our national coverage. This brand new email newsletter will distill all of our very best stories from our reporter in Ottawa, and stories from our pool of national contributors. You’ll also be the first to know about breaking stories of national importance.</p>
-      <p>We will continue to offer our regular newsletters, which deliver all the stories posted on The Tyee. To see a full list of our newsletters, click <a href="http://subscribe.thetyee.ca" target="_blank">here</a>.</p>
-      <p>To support to The Tyee’s national reporting as a Tyee Builder, click <a href="http://support.thetyee.ca" target="_blank">here</a>.</p>
-    </div>
+
   </form>
 
 
 
 
-
 <?php
-$js_footer = "inc/footer.php";
-//$js_footer = "$_SERVER[DOCUMENT_ROOT]/inc/footer.php";
+$js_footer = "$_SERVER[DOCUMENT_ROOT]/inc/footer.php";
 if (file_exists($js_footer)){
    include ($js_footer);
 }
