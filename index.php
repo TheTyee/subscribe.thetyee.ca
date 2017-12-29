e96c4ce8930ab7edfb248e9470251bc7f9a8ce25
The Tyee <bryancarney@gmail.com>
2017-12-29 09:57:03 -0800

    subscribe form updates

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 
 
   <h1>Join The Tyee's mailing list</h1>
-  <form action="http://whatcounts.com/bin/listctrl" method="POST">
+  <form action="http://whatcounts.net/bin/listctrl" method="POST">
     <input type="hidden" name="slid" value="6B5869DC547D3D46CFC60A3DF69EE7EB" />
     <input type="hidden" name="cmd" value="subscribe" />
     <input type="hidden" name="goto" value="http://subscribe.thetyee.ca/success/" />
@@ -26,7 +26,7 @@
         </div>
       </div>Send me the weekly newsletter
       <div class="description">
-          Every Monday you'll get an email listing all stories from the last week, as well as polls, top comments and contests. You'll also get early notice on Tyee events, news, promotions, partner messages and special initiatives.
+          Every Monday you'll get an email listing all stories from the last week, as well as polls, top comments and contests. 
         </div>
     </label>
 
@@ -38,7 +38,7 @@
         </div>
       </div>Send me the daily newsletter
      <div class="description">
-        Every day you'll get an email with all stories from the last 24 hours, as well as polls and contests. You'll also get early notice on Tyee events, news, promotions, partner messages and special initiatives.
+        Every day you'll get an email with all stories from the last 24 hours, as well as polls and contests.
       </div>
     </label>
 
@@ -50,20 +50,21 @@
         </div>
       </div>Send me the national newsletter 
       <div class="description">
-     Sent out once a week, you’ll get our latest and best reporting on issues of national scope, plus occasional breaking story alerts. You'll also get early notice on Tyee events, news, promotions, partner messages and special initiatives.  </div>
+     Sent out once a week, you’ll get our latest and best reporting on issues of national scope, plus occasional breaking story alerts. </div>
     </label>
 
-    <label class="checkbox">
-      <div role="presentation" class="form-checkbox">
-        <div class="radio-buttons">
-          <input type="radio" aria-checked="false" id="custom_pref_sponsors-yes" name="custom_pref_sponsor_casl" value="1"/> Yes
-          <input type="radio" aria-checked="true" id="custom_pref_sponsors-no" name="custom_pref_sponsor_casl" value="0" checked /> No
-        </div>
-      </div>Send me messages from select Tyee sponsors
-      <div class="description">
-        Messages from select sponsors we think you might be interested in. Simply having you on this list is a no-cost way to help fund more indie journalism.
-      </div>
+
+
+
+
+<label class="checkbox" style="cursor:auto;">
+          <div class="description">
+     <em style="color:#4F7519;">You'll also get early notice on Tyee events, news, promotions, partner messages and special initiatives. What's a partner message? A partner could be a carefully considered paid sponsor, or a group that we think you'd be interested in hearing from. We send these no more than once a month, and you are free to opt out of receiving partner messages. Simply having you on this list is a no-cost way to help fund more indie journalism.</em> </div>
     </label>
+
+ 
+          <input type="hidden"  id="custom_pref_sponsors-yes" name="custom_pref_sponsor_casl" value="1" checked /> 
+ 
 
    <input type="submit" value="Join">
 
