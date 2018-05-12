4e1540f7e427ac588f8cb585fc0d8b16f53e1a88
Phillip Smith <ps@phillipadsmith.com>
2018-05-12 13:59:55 -0700

    Update copy

--- success/index.php
+++ success/index.php
@@ -9,9 +9,11 @@
 	<h1>Thank you for subscribing</h1>
 
 	<div class="main-content">
-		<p>Your email has been added to our system and you will be e-mailed shortly with a request to confirm your subscription.</p>
-		<p>You must confirm your subscription if you want to receive e-mails from The Tyee (thetyee@response.thetyee.ca).</p>
-		<p>Please take a moment to do that now. You may want to look in your junk folder if you can't find the message at first.</p>
+		<p>Your email has been added to our system and you will be e-mailed shortly with a welcome message.</p>
+
+		<p>To ensure receive e-mails from The Tyee (thetyee@response.thetyee.ca) please check for this message now to ensure it hasn't gone to you junk folder.</p>
+
+		<p>If the message arrived in your junk folder, please follow these instructions to at The Tyee to your address book.</p>
 	</div>
 
 <?php
